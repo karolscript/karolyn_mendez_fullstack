c7ae1e32c0b0003e91a94fa3d41bb33160f927fe
Karolyn <karolscript@gmail.com>
2024-03-20 15:52:30 -0400

    deleted unused imports

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,6 @@
-import Header from "../components/header/Header";
-import { useSession } from 'next-auth/react';
-import Layout from "../layout";
 import Search from "../components/search/search";
 
 const Home = () => {
-  
     return (
         <Search />
     );
